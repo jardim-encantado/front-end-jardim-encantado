652e081291edbc5de80381af7f728c6a345edb8d
rkhue <85773727+rkhue@users.noreply.github.com>
2026-03-12 01:12:29 -0300

    feat: refactor ProfessorInfo component for improved styling and remove console log

--- src/components/ProfessorInfo/ProfessorInfo.jsx
+++ src/components/ProfessorInfo/ProfessorInfo.jsx
@@ -1,11 +1,11 @@
+import { useState } from "react";
+import Cabecalho from "../EstudanteInfo/Cabecalho";
 import InfoRow from "../EstudanteInfo/./InfoLinha";
 import styles from "./ProfessorInfo.module.css";
 
 export default function ProfessorInfo({ teacher, onDelete }) {
 
   const [editando, setEditando] = useState(false);
-
-  console.log(teacher);
 
   const teacherName = teacher?.fullName || teacher?.name || "-";
   const teacherEmail = teacher?.email || "-";
@@ -16,19 +16,19 @@ export default function ProfessorInfo({ teacher, onDelete }) {
 
   return (
     <div
-      className={styles.teacherCard}
+      className={styles.card}
     >
 
       <div
-        className={styles.teacherCardButtons}
+        className={styles.actions}
       >
         <button onClick={() => setEditando(!editando)}>✏️</button>
-        <button onClick={onDelete}>🗑</button>
+        <button onClick={onDelete}>🗑️</button>
       </div>
 
       <Cabecalho title="Detalhes do Professor" />
 
-      <div style={{ padding: "20px", display: "flex", gap: "40px" }}>
+      <div className={styles.content}>
         <div>
           <InfoRow label="Nome:" value={teacherName} disabled={!editando} />
           <InfoRow label="Email:" value={teacherEmail} disabled={!editando} />
